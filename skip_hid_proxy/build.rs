83001aef1339d08d0c91c68e28df9cf9b61b8e82
Valentijn Hol <git@hirtol.top>
2024-05-02 23:21:48 +0200

    Disable x86 build due to Frida being annoying

--- skip_hid_proxy/build.rs
+++ skip_hid_proxy/build.rs
@@ -1,5 +1,4 @@
 fn main() {
     forward_dll::forward_dll("C:\\Windows\\System32\\version.dll").unwrap();
-    forward_dll::forward_dll("C:\\Windows\\System32\\bcrypt.dll").unwrap();
     forward_dll::forward_dll("C:\\Windows\\System32\\hid.dll").unwrap();
 }
